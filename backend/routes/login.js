120d6c3ee1a28b678c7081e55e193bdb04173abe
dragontho <dragontho@gmail.com>
2022-03-23 17:10:35 +0800

    added get_cart and add_item functionality

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -6,7 +6,6 @@ const auth = require('../utils/auth');
 const connection = require('../utils/connection');
 
 
-/* GET users listing. */
 router.post('/', function(req, res, next) {
     let user_name = req.body.username;
     let passw = req.body.password;
@@ -17,15 +16,16 @@ router.post('/', function(req, res, next) {
             const verify_username = rows[0].username;
             const verify_password = rows[0].password;
             if (verify_username === user_name && verify_password === passw) {
-                const token = auth.encode_jwt(user_name);
+                const token = auth.encode_jwt({username: user_name});
                 res.json({token: token});
             } else {
                 res.json({error: "This should not be here"});
             }
         } else {
-            res.json({error: "User does not exist!"})
+            res.status(401).json({error: "User does not exist!"});
         }
     });
+
 });
 
 module.exports = router;
--- backend/routes/shopping.js
+++ backend/routes/shopping.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const auth = require('../utils/auth');
 const connection = require('../utils/connection');
+const mysql = require('mysql2/promise');
 
 router.get('/get_categories', function(req, res, next) {
     connection.query('SELECT * FROM category', (err, rows, fields) => {
@@ -10,15 +11,52 @@ router.get('/get_categories', function(req, res, next) {
     });
 });
 
-router.get('/get_products', function(req, res, next) {
-    connection.query('SELECT * FROM product', (err, rows, fields) => {
-        if (err) throw err;
-        res.json(rows);
-    });
+router.get('/get_products', async function(req, res, next) {
+    // connection.query('SELECT * FROM product', (err, rows, fields) => {
+    //     if (err) throw err;
+    //     res.json(rows);
+    // });
+    const con = await mysql.createConnection({host:'localhost', user: 'root', password:'paultho', database: 'prac'});
+    const [rows, fields] = await con.execute('SELECT * FROM product');
+    res.json(rows);
 });
 
-// router.get('/get_cart', auth.auth_jwt, function(req, res, next)  {
-//     connection.query()
-// });
+router.get('/get_cart', auth.auth_jwt, async function(req, res, next)  {
+    const access_token = req.headers.access_token;
+    const {username} = auth.decode_jwt(access_token);
+    const con = await mysql.createConnection({host:'localhost', user: 'root', password:'paultho', database: 'prac'});
+    const [rows, fields] = await con.execute('SELECT id FROM customer WHERE username=?', [username]);
+    if (rows.length === 0) {
+        return res.status(401).json({error: 'This should not be here'});
+    }
+    const cust_id = rows[0].id;
+    const [orders, _] = await con.execute('SELECT id FROM order_ WHERE customer_id=? AND status="Pending"', [cust_id]);
+    if (orders.length === 0) {
+        return res.json([]);
+    }
+    const ord_id = orders[0].id;
+    const [cart, __] = await con.execute('SELECT * FROM orderitem WHERE order_id=?', [ord_id]);
+    res.json(cart);
+});
+
+router.post('/add_item', auth.auth_jwt, async function(req, res, next) {
+    const access_token = req.headers.access_token;
+    const {username} = auth.decode_jwt(access_token);
+    const {product_id, product_qty} = req.body;
+    const con = await mysql.createConnection({host:'localhost', user: 'root', password:'paultho', database: 'prac'});
+    const [rows, fields] = await con.execute('SELECT id FROM customer WHERE username=?', [username]);
+    if (rows.length === 0) {
+        return res.status(401).json({error: 'This should not be here'});
+    }
+    const cust_id = rows[0].id;
+    const [orders, _] = await con.execute('SELECT id FROM order_ WHERE customer_id=? AND status="Pending"', [cust_id]);
+    if (orders.length === 0) {
+        const[orders, _] = await con.execute('INSERT INTO order_(customer_id, status) VALUES (?, "Pending"); SELECT id FROM order_ WHERE customer_id=? AND status="Pending"', [cust_id, cust_id]);
+    }
+    const ord_id = orders[0].id;
+    const [price, __] = await con.execute('SELECT price FROM product WHERE id=?', [product_id]);
+    await con.execute('INSERT INTO orderitem(product_id, order_id, product_qty, total_price) VALUES (?, ?, ?, ?)', [product_id, ord_id, product_qty, parseInt(price[0].price)*product_qty]);
+    res.status(200).json({response: "Added " + product_id + "x" + product_qty + " to cart"});
+});
 
 module.exports = router;
